f6a8b15a991e287b0a1d39f8a0076b71ca751678
kunljha <jha711kunal@gmail.com>
2022-02-12 14:18:26 +0530

    Header Component

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+	<button
+		class="btn"
+		v-bind:style="{
+			backgroundColor: color,
+		}"
+	>
+		{{ text }}
+	</button>
+</template>
+
+<script>
+export default {
+	name: 'Button',
+	props: {
+		text: String,
+		color: String,
+	},
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+	<header>
+		<h1>{{ title }}</h1>
+		<Button text="Add Task" color="green" />
+	</header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+	name: 'Header',
+	components: {
+		Button,
+	},
+	props: {
+		title: String,
+	},
+}
+</script>
+
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 20px;
+}
+</style>
